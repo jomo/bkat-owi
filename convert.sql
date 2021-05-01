6f3cabf53943d156df377efe3f381467e6a9ea3a
jomo <git@jomo.tv>
2021-05-01 02:09:48 +0200

    trim all whitespace

--- convert.sql
+++ convert.sql
@@ -6,21 +6,21 @@
 .headers on
 .output data.csv
 SELECT
-  tbnr AS Tatbestandsnummer,
+  trim(tbnr) AS Tatbestandsnummer,
   trim(tdb1 || " " || tdb2 || " " || tdb3 || " " || tdb4 || " " || tdb5) AS Tatbestand_Datenbank,
   trim(pdb1 || " " || pdb2) AS Paragraphen_Datenbank,
-  fap AS FaP,
-  p AS P,
+  trim(fap) AS FaP,
+  trim(p) AS P,
   cast(euro || "." || cent as float) AS Euro,
-  klt1 || " " || klt2 || " " || klt3 AS Klassifizierung_Text,
-  von AS Gueltig_bis,
-  bis AS Gueltig_bis,
-  kr1 AS Konkretisierung1,
-  kr2 AS Konkretisierung2,
-  kl1 AS Klassifizierung1,
-  kl2 AS Klassifizierung2,
-  kl3 AS Klassifizierung3,
-  kl4 AS Klassifizierung4,
+  trim(klt1 || " " || klt2 || " " || klt3) AS Klassifizierung_Text,
+  trim(von) AS Gueltig_bis,
+  trim(bis) AS Gueltig_bis,
+  trim(kr1) AS Konkretisierung1,
+  trim(kr2) AS Konkretisierung2,
+  trim(kl1) AS Klassifizierung1,
+  trim(kl2) AS Klassifizierung2,
+  trim(kl3) AS Klassifizierung3,
+  trim(kl4) AS Klassifizierung4,
   trim(tdr1 || char(10) || tdr2 || char(10) || tdr3 || char(10) || tdr4 || char(10) || tdr5, " " || char(10)) AS Tatbestand_Druckdatei,
   trim(pdr1 || " " || pdr2) AS Paragraphen_Druckdatei
 FROM bat;
