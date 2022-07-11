25c5c4f04de94b328fdafae5b70ce6cdad2ff542
jomo <git@jomo.tv>
2022-07-11 04:09:45 +0200

    use more accurate column names

--- convert.sh
+++ convert.sh
@@ -10,7 +10,7 @@ iconv -f Windows-1252 -t UTF-8 data.txt > data_unix.txt
 dos2unix data_unix.txt
 
 echo "Adding CSV Header"
-echo 'tbnr^tdb1^tdb2^tdb3^tdb4^tdb5^pdb1^pdb2^fap^p^euro^cent^klt1^klt2^klt3^bis^von^kr1^kr2^kl1^kl2^kl3^kl4^tdr1^tdr2^tdr3^tdr4^tdr5^pdr1^pdr2' | cat - data_unix.txt > input.txt
+echo 'tbnr^tdb1^tdb2^tdb3^tdb4^tdb5^pdb1^pdb2^fap^p^euro^cent^klt1^klt2^klt3^bis^von^fv^kr^kl^tab^unter^ober^tdr1^tdr2^tdr3^tdr4^tdr5^pdr1^pdr2' | cat - data_unix.txt > input.txt
 rm data_unix.txt
 
 echo "Parsing and converting to CSV"
